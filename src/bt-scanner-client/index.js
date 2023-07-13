48eea84e77d0953eaa589d9cd401f0cc2770c106
Bishop Zareh <xchicago@gmail.com>
2023-07-13 13:42:05 -0600

    continued work on socket.io

--- src/bt-scanner-client/index.js
+++ src/bt-scanner-client/index.js
@@ -18,11 +18,11 @@ const socket = io(serverAddress);
 console.log('connecting to ' + serverAddress)
 
 socket.on("connect", () => {
-  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+  console.log('socket open ' + socket.id); // x8WIv7-mJelg7on_ALbx
 });
 
 socket.on("disconnect", () => {
-  console.log(socket.id); // undefined
+  console.log('socket closed'); // undefined
 });
 
 
@@ -35,7 +35,7 @@ noble.on('stateChange', async (state) => {
 
 noble.on('discover', async (peripheral) => {
   // if (peripheral.advertisement.localName)
-  //   console.log('Found: ' + peripheral.advertisement.localName)
+    console.log(peripheral.address, peripheral.uuid, peripheral.advertisement.localName)
 
   if (
     peripheral.uuid === 'ee205aea6ae6063b179b455255982909' // mac
